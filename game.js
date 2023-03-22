befea24fdaeae8967909ab5fa76330695a233995
MacaGQ <macarenadgq@gmail.com>
2023-03-21 23:35:59 -0300

    added new div for every try

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 function randomNumGenerator() {
     let array = []
-    while(array.length < 4) {
+    while (array.length < 4) {
         let number = Math.floor((Math.random() * 9));
-        if (!array.includes(number)){
+        if (!array.includes(number)) {
             array.push(number)
         }
     }
@@ -27,32 +27,54 @@ const fourthDot = document.getElementById('fourth-dot');
 
 dotArray = [firstDot, secondDot, thirdDot, fourthDot];
 
-for(let i in dotArray){
+for (let i in dotArray) {
     dotArray[i].style.backgroundColor = pickedColors[i];
 }
 
 var dots = Array.from(document.getElementsByClassName('dot hidden'))
 
-function showDots(){
-        dots.forEach(element => element.classList.toggle('hidden'));
-    }
+function showDots() {
+    dots.forEach(element => element.classList.toggle('hidden'));
+}
 
 
 inputDots = Array.from(document.getElementsByClassName('input'))
 
-for (let i in inputDots){
+for (let i in inputDots) {
     inputDots[i].style.backgroundColor = randomColorGen(parseInt(i));
 }
 
 selectedColors = Array.from(document.getElementsByClassName('selected'))
 
 var count = 0
+var codeTry = []
 
 function colorSelect() {
-    if (count<4){
+    if (count < 4) {
         selectedColors[count].style.backgroundColor = `var(--${this.id})`
-    count ++
+        codeTry.push(this.id)
+        count++
     }
 }
 
 inputDots.forEach(element => element.addEventListener('click', colorSelect))
+
+function clearColors(){
+    selectedColors.forEach(element => element.style.backgroundColor = 'white')
+    count = 0;
+    codeTry = []
+}
+
+function createDiv() {
+    const oldDiv = document.getElementById('currentSelection')
+    const newDiv = oldDiv.cloneNode(true)
+    newDiv.classList.add('playground')
+    newDiv.id = '';
+    const newNode = document.getElementById('game').insertBefore(newDiv, document.getElementById('currentSelection'))
+}
+
+function play(){
+    console.log(codeTry)
+    createDiv()
+    clearColors()
+}
